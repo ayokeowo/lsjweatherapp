675f64052325339403c5926c85adcb03617fd3e0
Ayo <okeowokunle@gmail.com>
2020-12-28 14:26:42 +0000

    new storage for remote state file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tforming"
-    storage_account_name = "tformingbackend"
+    resource_group_name  = "terraformthings"
+    storage_account_name = "terraformthingsstorage"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
