eab6c60f82843abf3107baaf5d345b2d74e4da28
ayokunle99 <45292938+ayokunle99@users.noreply.github.com>
2020-12-28 20:41:25 +0000

    updated backend again

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "terraformthings"
     storage_account_name = "terraformthingsstorage"
-    container_name       = "tfstates123"
+    container_name       = "tf123"
     key                  = "prod.terraform.tfstate"
   }
 }
